fe73aa3251b0818bc44b5f13838b40deedd289f5
d9beuD <53569029+d9beuD@users.noreply.github.com>
2020-04-14 15:19:41 +0200

    Configuration de Webpack pour éviter les problèmes de cache

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,32 @@
 module.exports = {
+  chainWebpack: config => {
+    if (process.env.NODE_ENV === 'development') {
+      config
+        .output
+        .filename('[name].[hash].js')
+        .end()
+    }
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .loader('vue-loader')
+      .tap(options => {
+        options.transformAssetUrls = {
+          img: 'src',
+          image: 'xlink:href',
+          'b-img': 'src',
+          'b-img-lazy': ['src', 'blank-src'],
+          'b-card': 'img-src',
+          'b-card-img': 'src',
+          'b-card-img-lazy': ['src', 'blank-src'],
+          'b-carousel-slide': 'img-src',
+          'b-embed': 'src'
+        }
+
+        return options
+      })
+  },
   pluginOptions: {
     i18n: {
       locale: 'fr',
