b7538b39f4e27a059c3f421498c8ba4f37d57ea7
d9beuD <53569029+d9beuD@users.noreply.github.com>
2020-04-14 11:03:41 +0200

    Installation de dépendances manquantes

--- src/font-awesome.js
+++ src/font-awesome.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
-import { fas, far, fal, fad, fab } from '@fortawesome/fontawesome-pro'
+import { fas } from '@fortawesome/pro-solid-svg-icons'
+import { far } from '@fortawesome/pro-regular-svg-icons'
+import { fal } from '@fortawesome/pro-light-svg-icons'
+import { fad } from '@fortawesome/pro-duotone-svg-icons'
 
-library.add(fas, far, fal, fad, fab)
+library.add(fas, far, fal, fad)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('font-awesome-layers', FontAwesomeLayers)
