42f65ef38bcbcb6c4e5d5c28a3ccd63cc5b05bfa
d9beuD <53569029+d9beuD@users.noreply.github.com>
2020-04-15 14:18:32 +0200

    Ajout de styles

--- src/views/Conversation.vue
+++ src/views/Conversation.vue
@@ -3,9 +3,23 @@
     <div class="p-2 text-center bg-white border-bottom">
       {{ conversation.pseudo }}
     </div>
-    <div class="stretch-scroll p-2">
-      <div v-for="(message, index) in conversation.messages" :key="index">
-        {{ message.content }}
+    <div ref="scroll" class="stretch-scroll p-2">
+      <div
+        class="d-flex"
+        :class="{'justify-content-end': message.from =='me', 'mt-2': (index > 0 ? conversation.messages[index - 1].from != message.from ? true:false:false)}"
+        v-for="(message, index) in conversation.messages"
+        :key="index"
+        >
+        <div v-if="message.from != 'me'">
+          <b-avatar class="mr-1" :text="conversation.pseudo.charAt(0)" size="2rem" />
+        </div>
+        <div
+          class="px-2 py-1 border"
+          :class="['bg-' + (message.from == 'me' ? 'primary':'300'), 'text-' + (message.from == 'me' ? 'white':'dark')]"
+          :style="{borderRadius: '15px', maxWidth: '75%'}"
+          >
+          {{ message.content }}
+        </div>
       </div>
     </div>
     <div class="border-top p-2">
@@ -51,6 +65,13 @@ export default {
     onSubmit () {
       this.sendMessage({ id: this.id, content: this.message })
       this.message = ''
+    }
+  },
+  watch: {
+    'conversation.messages' () {
+      this.$nextTick(() => {
+        this.$refs.scroll.scrollTop = this.$refs.scroll.clientHeight
+      })
     }
   }
 }
