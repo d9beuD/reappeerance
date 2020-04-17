a305aff0f4ecdd925ee2abd1c377d27b704cc205
d9beuD <53569029+d9beuD@users.noreply.github.com>
2020-04-17 09:46:52 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -56,6 +56,17 @@ module.exports = {
           target: 'portable'
         }
       }
-    }
+    },
+    chainWebpackMainProcess: config => {
+      // Chain webpack config for electron main process only
+    },
+    chainWebpackRendererProcess: config => {
+      // Chain webpack config for electron renderer process only
+      // The following example will set IS_ELECTRON to true in your app
+      config.plugin('define').tap(args => {
+        args[0]['IS_ELECTRON'] = true
+        return args
+      })
+    },
   }
 }
