88868e94198133ecf1481f2cb4121a043c23afb1
d9beuD <53569029+d9beuD@users.noreply.github.com>
2020-04-17 19:04:03 +0200

    Ajout d'un badge quand une conversation n'est pas lue

--- src/components/ConversationListItem.vue
+++ src/components/ConversationListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <b-list-group-item :to="{ name: 'Conversation', params: { id: conv.id }}" active-class="bg-200">
+  <b-list-group-item :to="{ name: 'Conversation', params: { id: conv.id }}" active-class="bg-200" class="px-1">
   <div class="row no-gutters align-items-center">
     <div class="col-auto">
       <b-avatar class="mr-1">
@@ -12,11 +12,16 @@
       </div>
         <small class="text-muted">{{ lastMessage.content }}</small>
     </div>
+    <div class="col-auto d-flex mr-1" v-if="(lastMessage.from != me) && (!lastMessage.read) && (this.conv.messages.length > 0)">
+      <div class="rounded-pill bg-primary" :style="{height: '.5rem', width:'.5rem'}"></div>
+    </div>
   </div>
 </b-list-group-item>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   props: {
     conv: {
@@ -24,6 +29,9 @@ export default {
     }
   },
   computed: {
+    ...mapState('conversation', {
+      me: 'identifier'
+    }),
     lastMessage () {
       return (this.conv.messages.length > 0)
         ? this.conv.messages[this.conv.messages.length - 1] : ''
