eaf2faad4f782baeb74c804dbc316d404920f28a
d9beuD <53569029+d9beuD@users.noreply.github.com>
2020-04-17 17:31:20 +0200

    Ajout du dernier message dans la liste des conversations

--- src/components/ConversationList.vue
+++ src/components/ConversationList.vue
@@ -1,34 +1,23 @@
 <template>
   <b-list-group flush>
-    <b-list-group-item
-      class="px-1"
+    <conversation-list-item
       v-for="conv in conversations"
       :key="conv.id"
-      :to="{ name: 'Conversation', params: { id: conv.id }}"
-      >
-        <div class="row no-gutters align-items-center">
-          <div class="col-auto">
-            <b-avatar class="mr-1">
-              {{ conv.identity.pseudo.charAt(0) }}
-            </b-avatar>
-          </div>
-          <div class="col text-truncate" :style="{lineHeight: '1rem', minWidth: 0}">
-            <div>
-              {{ conv.identity.pseudo }}
-            </div>
-              <small>Message</small>
-          </div>
-        </div>
-    </b-list-group-item>
+      :conv="conv"
+      />
   </b-list-group>
 </template>
 
 <script>
 import { mapState } from 'vuex'
+import ConversationListItem from './ConversationListItem'
 
 export default {
   computed: {
     ...mapState('conversation', ['conversations'])
+  },
+  components: {
+    ConversationListItem
   }
 }
 </script>
---
+++ src/components/ConversationListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-list-group-item :to="{ name: 'Conversation', params: { id: conv.id }}" active-class="bg-200">
+  <div class="row no-gutters align-items-center">
+    <div class="col-auto">
+      <b-avatar class="mr-1">
+        {{ conv.identity.pseudo.charAt(0) }}
+      </b-avatar>
+    </div>
+    <div class="col text-truncate" :style="{lineHeight: '1rem', minWidth: 0}">
+      <div>
+        {{ conv.identity.pseudo }}
+      </div>
+        <small class="text-muted">{{ lastMessage.content }}</small>
+    </div>
+  </div>
+</b-list-group-item>
+</template>
+
+<script>
+export default {
+  props: {
+    conv: {
+      type: Object
+    }
+  },
+  computed: {
+    lastMessage () {
+      return (this.conv.messages.length > 0)
+        ? this.conv.messages[this.conv.messages.length - 1] : ''
+    }
+  }
+}
+</script>
